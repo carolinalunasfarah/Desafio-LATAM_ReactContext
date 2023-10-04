02ed7e38aacbbcbf47aca16f25b41bb7dcb2cc6a
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-10-04 19:50:41 -0300

    photos context created with axios to get response from json

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+import PhotosProvider from "./components/context/PhotosContext.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <PhotosProvider>
+                <App />
+            </PhotosProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
