7936903522eacbdaf8419729e3e8b8ec047c0029
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-10-04 20:15:13 -0300

    gallery logic using map and addFavorite function

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,36 @@
+import { useContext } from "react";
+import { PhotosContext } from "../components/context/PhotosContext";
+import IconHeart from "./IconHeart";
+
 const Gallery = () => {
-  return <div className="gallery grid-columns-5 p-3"></div>;
+    const { photos, setPhotos } = useContext(PhotosContext);
+
+    const addFavorite = (id) => {
+        const photosNew = photos.map((photo) => {
+            if (photo.id === id) {
+                return { ...photo, isFavorite: true };
+            }
+            return photo;
+        });
+        setPhotos(photosNew);
+    };
+
+    return (
+        <div className="gallery grid-columns-5 p-3">
+            {photos.map((photo, p) => (
+                <section
+                    key={p}
+                    onClick={() => addFavorite(photo.id)}
+                    className="photoCard"
+                    style={{ backgroundImage: `url(${photo.src.large})` }}>
+                    <IconHeart filled={photo.isFavorite} />
+                    <article>
+                        <p>{photo.alt}</p>
+                    </article>
+                </section>
+            ))}
+        </div>
+    );
 };
+
 export default Gallery;
